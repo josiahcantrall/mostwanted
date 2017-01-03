4f26d0e7b15fc8d32480ab648572839bb884e7cf
devalexedmondson <alexedmondson20@gmail.com>
2017-01-03 15:57:04 -0600

    changed first function, added var split one

--- app.js
+++ app.js
@@ -11,32 +11,40 @@ printAllToConsole(data);
 */
 
 function initSearch(data){
-	alert("Would you like to search by name?");
+    var yourName = prompt("Do you want to search by 'name' or 'other criteria'?");
+    var name = splitone(name)
+    }
 
-	// get all the information you need to run the search
-	var yourName = prompt("Who do you want to search for?");
+function splitone(data){
 
-	// then pass that info to the respective function.
-	var result = getPersonInfo("J", "T")
+    // get all the information you need to run the search
+    var yourName = prompt("Who do you want to search for?");
 
-	// once the search is done, pass the results to the responder function
-	responder(result);
+    // then pass that info to the respective function.
+    var result = getPersonInfo("J", "T")
+
+    // once the search is done, pass the results to the responder function
+    responder(result);
 }
 
+
+
+
+
 function responder(results){
-	// results may be a list of strings, an object, or a single string.
-	alert(results);
+    // results may be a list of strings, an object, or a single string.
+    alert(results);
 }
 
 function getPersonInfo(firstname, lastname){
-	var result = "This will be the information for whoever you searched for";
-	// return the object of a person
-	return result;
+    var result = "This will be the information for whoever you searched for";
+    // return the object of a person
+    return result;
 }
 
 function getFamily(){
-	// return an array containing the members of the person's family
-	// the array should contain each person's unmodified object
+    // return an array containing the members of the person's family
+    // the array should contain each person's unmodified object
 }
 
 // there will be much more here, and some of the code above will certainly change
