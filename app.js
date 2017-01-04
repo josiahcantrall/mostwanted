62de9c5932d931e3428129e34f81868bf461de26
josiahcantrall <josiahcantrall@hotmail.com>
2017-01-04 13:40:34 -0600

    trying to merge files so I can pull Alex data

--- app.js
+++ app.js
@@ -1,112 +1,74 @@
-
-/*  Remove this from your final submission
-function printAllToConsole(dataObj){
-	for (var key in dataObj) {
-		if (dataObj.hasOwnProperty(key)) {
-			console.log(key + " -> " + JSON.stringify(dataObj[key]));
-		}
-	}
+function initMostWanted(people){
+    alert("Welcome to Most Wanted. Follow the prompts to search for infromation on the person you seek.");
+    do{
+        var searchType = prompt ("Do you want to search by name or attributes? Enter the word 'name' or 'attributes'.");
+    }while(!(searchType== "name" || searchType == "attributes"));
+    switch(searchType){
+        case "name":
+        var person = getPerson(prompt("Enter person's first name."), prompt("Enter person's last name."), people);
+        mainMenu(person, people);
+        break;
+        case "attributes":
+        break;
+        default:
+        alert("There was an error processing your request.");
+        initMostWanted(people);
+    }
+    
 }
-printAllToConsole(data);
-*/
 
-function initSearch(){
-    var yourName = prompt("Do you want to search by 'name' or 'other criteria'?");
-    if(yourName === 'name'){
-        splitOne();
-    }
-    
-    else if (yourName === 'other criteria') {
-        splitTwo();
-    }
-    
-    else {
-        alert("Wrong user input")
-        initSearch();
-    }
-    }
+function getPerson(firstName, lastName, people) {
+    // body...
+}
 
-function splitOne(){
+function mainMenu (person, people) {
 
-    // get all the information you need to run the search
-    var firstName = prompt("Who do you want to search for, enter first name");
-    
-    if (firstName === " "){ 
-        //need function to ask user to enter name from data to move on 
-        lastName();
-    }
-    else{
-        alert ("Wrong user input")
-        splitOne();
-    }
+
+if(!person){
+    alert("could not find individual.");
+    return initMostWanted(people);
+
+var displayOption = prompt("Found " + person.firstName + " " + person.lastName + ". Do you want to know their 'info', 'family', next of 'kin', or 'descendants'? Type the option you want or 'restart' or 'quit'.");
+
+switch(displayOption){
+   case "info":
+
+       break;
+
+   case "family":
+
+       break;
+
+   case "kin":
+
+       break;
+
+   case "descendants":
+
+       break;
+   
+   case "restart":
+       initSearch(people)
+       break;
+
+   case "quit":
+       return;
+       break;
+       
+
+   default:
+       return mainMenu(person,people);
+}
+
+}
+
+
+//After finding someone, this will display all of their info!
+function getInfo(person,people){
+   alert("Person." + person.firstName + " " + person.lastName+ "");
+   mainMenu(person,people)
+}
         
    
 
-    // then pass that info to the respective function.
-    var result = getPersonInfo("J", "T")
 
-    // once the search is done, pass the results to the responder function
-    responder(result);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-function splitTwo(){
-<<<<<<< HEAD
-    var personCriteria = prompt("Please answer the following questions to find who you want. What is the persons aprox. age? Ex. Between the ages of 14-50")
-    var gender = prompt("What is the person's gender? Ex. 'male' or 'female'")
-    var dob = prompt("What is the person's date of birth? Ex. 1/18/1949 or NA if unknown")
-    var height = prompt("What is the person's aprox height in inches? Ex. 71")
-    var weight = prompt("What is the person's aprox height in inches? Ex. 71")
-    var eyeColor = prompt("What is the person's aprox height in inches? Ex. 71")
-    var occupation = prompt("What is the person's aprox height in inches? Ex. 71")
-=======
->>>>>>> 84ef011d74abdb732f02e31ec86f7531ef4fde5c
-    
-    alert("Please answer the following questions, if you do not know the answer please type Unknown")
-    
-    var personGender = prompt("What is the persons gender, male or female?")
-    
-    var personAge = prompt("How old is the person you are looking for?")
-    
-    var personAgeRange = prompt("What is the persons aprox. age? Example: 14-50")
-    
-   var personheight = prompt("What is the person's aprox height in inches? Ex. 71")
-    
-    var personWeight = prompt("How much does the person weigh? Please only put in the number format in lbs. Example: 175")
-    
-    var personOccupation = prompt("What is the persons occupation? Please only put in one word. Example: doctor")
-    
-    var personEyeColor = prompt("What is the persons eye color? Please only put in one word. Example: green")
-}
-
-
-
-
-function responder(results){
-    // results may be a list of strings, an object, or a single string.
-    alert(results);
-}
-
-function getPersonInfo(firstname, lastname){
-    var result = "This will be the information for whoever you searched for";
-    // return the object of a person
-    return result;
-}
-
-function getFamily(){
-    // return an array containing the members of the person's family
-    // the array should contain each person's unmodified object
-}
-
-// there will be much more here, and some of the code above will certainly change
