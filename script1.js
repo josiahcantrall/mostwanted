fc00a3b7a842fa599404ed186761ce1b7cc2d082
josiahcantrall <josiahcantrall@hotmail.com>
2017-01-10 12:25:13 -0600

    delted 3 unneccessary files that were used for testing purposes

--- script1.js
+++
@@ -1,1 +0,0 @@
- functionhanoi (height, first, temp, final ){ if ( height <= 0){ return;} hanoi ( height-1, first, final, temp ); console.log ( "Move disk " + height + " from " + first + " to " + final ); hanoi ( height-1, temp, first, final ); 
